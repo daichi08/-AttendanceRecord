7a9abd4a92693230cf9a2ce264f2748759b70e44
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2018-05-01 09:19:36 +0100

    バグ消したり

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -5,13 +5,15 @@ class MembersController < ApplicationController
   end
 
   def update
-    if params[:id].length == 1
-      changed_user = Member.find(params[:id])
-      changed_user.update(status: params[:status])
-    else
-      ids = params[:id].scan(/\w+/)
-      status_array = Array.new(ids.size, { status: params[:status] })
-      Member.update(ids, status_array)
+    unless params[:id].to_i.zero?
+      if params[:id].length == 1
+        changed_user = Member.find(params[:id])
+        changed_user.update(status: params[:status])
+      else
+        ids = params[:id].scan(/\w+/)
+        status_array = Array.new(ids.size, { status: params[:status] })
+        Member.update(ids, status_array)
+      end
     end
     redirect_to :root
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "members#index"
-  resources :members, only: %w[index update]
+  resources :members, only: %w[update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
